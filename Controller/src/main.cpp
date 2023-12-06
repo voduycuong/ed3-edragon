5ab93ba7fe638ba235c80d6a2bdeb281e039832e
Anh <anhtm621@gmail.com>
2023-12-06 22:54:12 +0700

    initialized code for controller

--- Controller/src/main.cpp
+++ Controller/src/main.cpp
@@ -1,25 +1,39 @@
 #include <Arduino.h>
-#include <ESP32Servo.h>
-#include <esp_now.h>
 #include <WiFi.h>
+#include <esp_now.h>
+#include <ESP32Servo.h>
+#include <Joystick_ESP32S2.h>
+
 // ================================================================
 // Variable declaration
 // ================================================================
-#define MAX_SIGNAL 2000 // Parameter required for the ESC definition
-#define MIN_SIGNAL 1000 // Parameter required for the ESC definition
-#define MOTOR_PIN 13    // Pin 13 attached to the ESC signal pin
+#define POT_PIN 34 // Pin 36 attached to the potentiometer
+#define JOYSTICK_PIN_SW	33
+#define JOYSTICK_PIN_VRX 35
+#define JOYSTICK_PIN_VRY 32
+#define PUSHBUTTON_1 39
+#define PUSHBUTTON_2 36
 
-// Define the incoming data, RECEIVED into this board
-typedef struct struct_msg_Receive
+// Insert the MAC address of the other board
+uint8_t broadcastAddress[] = {0x48, 0xE7, 0x29, 0xA0, 0x11, 0x98};
+
+// Define the outgoing data, SENT out from this board
+typedef struct struct_msg_Sent
 {
-    int Receive_PotValue;
-} struct_msg_Receive;
+    int Sent_PotAngle;
+} struct_msg_Sent;
 
 // Declare the structure
-struct_msg_Receive Receive_Data;
+struct_msg_Sent Sent_Data;
+
+// Variable for espnow communication
+esp_now_peer_info_t peerInfo;
 
 // Serial
 unsigned long time_prev_serial = 0;
+
+// Object initialization
+Joystick_ joystick(JOYSTICK_PIN_VRX, JOYSTICK_PIN_VRY, JOYSTICK_PIN_SW);
 
 Servo ESC;                   // Define the ESC
 int CtrlPWM;                 // Control Signal. Varies between [0 - 180]
@@ -30,56 +44,45 @@ unsigned long time_prev = 0; // Variable used for serial monitoring
 void SerialDataPrint();  // Function to print data on the serial monitor
 void Init_Serial();      // Function to init the serial monitor
 void WaitForKeyStroke(); // Function to interact with the serial monitor
-
 void SerialDataWrite();
-void OnDataReceive(const uint8_t *mac, const uint8_t *incomingData, int len);
-float floatMap(float, float, float, float, float);
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
 void espnow_initialize();
 
 // ================================================================
 // Setup
 // ================================================================
-void setup()
-{
-    Init_Serial();                                 // Initialize the serial communication
-    ESC.attach(MOTOR_PIN, MIN_SIGNAL, MAX_SIGNAL); // Initialize the ESC
-
-    
-
-    Serial.begin(115200);
-    espnow_initialize();
+void setup() {
+  Init_Serial(); // Initialize the serial communication
+  Serial.begin(115200);
+  espnow_initialize();
 }
 
 // ================================================================
 // Loop
 // ================================================================
-void loop()
-{
-    // CtrlPWM = map(analogRead(POT_PIN), 0, 4095, 0, 180); // Read the pot, map the reading from [0, 4095] to [0, 180]
-    CtrlPWM = Receive_Data.Receive_PotValue;
-    ESC.write(CtrlPWM); // Send the command to the ESC
+void loop() {
+  CtrlPWM = map(analogRead(POT_PIN), 0, 4095, 0, 180); // Read the pot, map the reading from [0, 4095] to [0, 180]
 
-    // Data Acquisition
-    Sent_Data.Sent_PotAngle = floatMap(Receive_Data.Receive_PotValue, 0, 4095, 0, 300);
-    // Data sent over espnow
-    esp_now_send(broadcastAddress, (uint8_t *)&Sent_Data, sizeof(Sent_Data));
+  // Data sent over espnow
+  esp_now_send(broadcastAddress, (uint8_t *)&Sent_Data, sizeof(Sent_Data));
 
-    if (micros() - time_prev_serial >= 20000)
-    {
-        time_prev_serial = micros();
-        SerialDataWrite();
-    }
+  if (micros() - time_prev_serial >= 20000)
+  {
+      time_prev_serial = micros();
+      SerialDataWrite();
+  }
 
-    SerialDataPrint(); // Print data on the serial monitor for debugging
+  SerialDataPrint(); // Print data on the serial monitor for debugging
 }
+
 // ================================================================
-// Function Definition
+// Function definition
 // ================================================================
 void Init_Serial()
 {
     Serial.begin(115200);
-    while (!Serial)
-        ;
+    while (!Serial);
 }
 // ================================================================
 void SerialDataPrint()
@@ -101,28 +104,23 @@ void WaitForKeyStroke()
         Serial.read();
 }
 
+// ******************************************
 void SerialDataWrite()
 {
     Serial.print(micros() / 1000);
     Serial.print("\t");
-    Serial.print(Receive_Data.Receive_PotValue);
+    Serial.print(Sent_Data.Sent_PotAngle);
     Serial.println();
 }
 
 // ******************************************
-void OnDataReceive(const uint8_t *mac, const uint8_t *incomingData, int len)
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
 {
-    // debugging serial
+    // There is nothing to do when sending data, this is just for debugging
     Serial.print(micros() / 1000);
-    Serial.println("\tData received!");
-    // You must copy the incoming data to the local variables
-    memcpy(&Receive_Data, incomingData, sizeof(Receive_Data));
-}
-
-// ******************************************
-float floatMap(float x, float in_min, float in_max, float out_min, float out_max)
-{
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    Serial.println("\tData sent!");
+    Serial.print("\r\nLast Packet Send Status:\t");
+    Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
 // ******************************************
@@ -134,5 +132,17 @@ void espnow_initialize()
         Serial.println("Error initializing ESP-NOW");
         return;
     }
-    esp_now_register_recv_cb(OnDataReceive);
+    esp_now_register_send_cb(OnDataSent);
+
+    // Register peer
+    memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+    peerInfo.channel = 0;
+    peerInfo.encrypt = false;
+
+    // Add peer
+    if (esp_now_add_peer(&peerInfo) != ESP_OK)
+    {
+        Serial.println("Failed to add peer");
+        return;
+    }
 }
