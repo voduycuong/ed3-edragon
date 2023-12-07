16bbe1b4ae08b55f5481632efd496cdd8b162c89
ngcmnh <s3907086@rmit.edu.vn>
2023-12-07 12:27:18 +0700

    can read joystick and 2 button

---
+++ .pio/build/esp32doit-devkit-v1/FrameworkArduino/FirmwareMSC.cpp.d
@@ -0,0 +1,5 @@
+.pio/build/esp32doit-devkit-v1/FrameworkArduino/FirmwareMSC.cpp.o: \
+ /Users/ngminh/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
+ /Users/ngminh/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
+ /Users/ngminh/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /Users/ngminh/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- Controller/src/main.cpp
+++ Controller/src/main.cpp
@@ -2,17 +2,19 @@
 #include <WiFi.h>
 #include <esp_now.h>
 #include <ESP32Servo.h>
-#include <Joystick_ESP32S2.h>
-
-// ================================================================
-// Variable declaration
-// ================================================================
+// #include <PluggableUSB.h>
+//  ================================================================
+//  Variable declaration
+//  ================================================================
 #define POT_PIN 34 // Pin 36 attached to the potentiometer
-#define JOYSTICK_PIN_SW	33
-#define JOYSTICK_PIN_VRX 35
-#define JOYSTICK_PIN_VRY 32
+#define JOYSTICK_PIN_SW 35
+#define JOYSTICK_PIN_VRX 32
+#define JOYSTICK_PIN_VRY 33
 #define PUSHBUTTON_1 39
 #define PUSHBUTTON_2 36
+
+int joy_vrx;
+int joy_vry;
 
 // Insert the MAC address of the other board
 uint8_t broadcastAddress[] = {0x48, 0xE7, 0x29, 0xA0, 0x11, 0x98};
@@ -21,6 +23,8 @@ uint8_t broadcastAddress[] = {0x48, 0xE7, 0x29, 0xA0, 0x11, 0x98};
 typedef struct struct_msg_Sent
 {
     int Sent_PotAngle;
+    bool Button1State;
+    bool Button2State;
 } struct_msg_Sent;
 
 // Declare the structure
@@ -33,7 +37,7 @@ esp_now_peer_info_t peerInfo;
 unsigned long time_prev_serial = 0;
 
 // Object initialization
-Joystick_ joystick(JOYSTICK_PIN_VRX, JOYSTICK_PIN_VRY, JOYSTICK_PIN_SW);
+// Joystick_ joystick(JOYSTICK_PIN_VRX, JOYSTICK_PIN_VRY, JOYSTICK_PIN_SW);
 
 Servo ESC;                   // Define the ESC
 int CtrlPWM;                 // Control Signal. Varies between [0 - 180]
@@ -52,28 +56,60 @@ void espnow_initialize();
 // ================================================================
 // Setup
 // ================================================================
-void setup() {
-  Init_Serial(); // Initialize the serial communication
-  Serial.begin(115200);
-  espnow_initialize();
+void setup()
+{
+    Init_Serial(); // Initialize the serial communication
+    Serial.begin(115200);
+    espnow_initialize();
+    pinMode(JOYSTICK_PIN_VRX, INPUT);
+    pinMode(JOYSTICK_PIN_VRY, INPUT);
+    pinMode(POT_PIN, INPUT);
+    pinMode(PUSHBUTTON_1, INPUT_PULLDOWN);
+    pinMode(PUSHBUTTON_2, INPUT_PULLDOWN);
 }
 
 // ================================================================
 // Loop
 // ================================================================
-void loop() {
-  CtrlPWM = map(analogRead(POT_PIN), 0, 4095, 0, 180); // Read the pot, map the reading from [0, 4095] to [0, 180]
+void loop()
+{
+    CtrlPWM = map(analogRead(POT_PIN), 0, 4095, 0, 180); // Read the pot, map the reading from [0, 4095] to [0, 180]
 
-  // Data sent over espnow
-  esp_now_send(broadcastAddress, (uint8_t *)&Sent_Data, sizeof(Sent_Data));
+    // Read Joystick Values
+    joy_vrx = analogRead(JOYSTICK_PIN_VRX);
+    joy_vry = analogRead(JOYSTICK_PIN_VRY);
 
-  if (micros() - time_prev_serial >= 20000)
-  {
-      time_prev_serial = micros();
-      SerialDataWrite();
-  }
+    // Read Button States
+    bool button1State = digitalRead(PUSHBUTTON_1) == HIGH; // Button is active-high
+    bool button2State = digitalRead(PUSHBUTTON_2) == HIGH; // Button is active-high
 
-  SerialDataPrint(); // Print data on the serial monitor for debugging
+    // Update Sent Data with Joystick and Button Values
+    Sent_Data.Sent_PotAngle = joy_vrx; // Send the joystick X value
+    Sent_Data.Button1State = button1State;
+    Sent_Data.Button2State = button2State;
+
+    // Data sent over espnow
+    esp_now_send(broadcastAddress, (uint8_t *)&Sent_Data, sizeof(Sent_Data));
+
+    if (micros() - time_prev_serial >= 20000)
+    {
+        time_prev_serial = micros();
+        SerialDataPrint(); // Print data on the serial monitor for debugging
+    }
+
+    // Print Joystick Values
+    if (millis() - time_prev >= 20000)
+    {
+        time_prev = millis();
+        Serial.print("Joystick X: ");
+        Serial.print(joy_vrx);
+        Serial.print("\tJoystick Y: ");
+        Serial.println(joy_vry);
+        Serial.print("\tButton 1: ");
+        Serial.print(button1State ? "Pressed" : "Released");
+        Serial.print("\tButton 2: ");
+        Serial.println(button2State ? "Pressed" : "Released");
+    }
 }
 
 // ================================================================
@@ -82,7 +118,8 @@ void loop() {
 void Init_Serial()
 {
     Serial.begin(115200);
-    while (!Serial);
+    while (!Serial)
+        ;
 }
 // ================================================================
 void SerialDataPrint()
@@ -92,7 +129,7 @@ void SerialDataPrint()
         time_prev = micros();
         Serial.print(millis());
         Serial.print("\t");
-        Serial.println(CtrlPWM);
+        // Serial.println(CtrlPWM);
     }
 }
 // ================================================================
@@ -109,7 +146,7 @@ void SerialDataWrite()
 {
     Serial.print(micros() / 1000);
     Serial.print("\t");
-    Serial.print(Sent_Data.Sent_PotAngle);
+    // Serial.print(Sent_Data.Sent_PotAngle);
     Serial.println();
 }
 
@@ -117,10 +154,10 @@ void SerialDataWrite()
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
 {
     // There is nothing to do when sending data, this is just for debugging
-    Serial.print(micros() / 1000);
-    Serial.println("\tData sent!");
-    Serial.print("\r\nLast Packet Send Status:\t");
-    Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+    // Serial.print(micros() / 1000);
+    // Serial.println("\tData sent!");
+    // Serial.print("\r\nLast Packet Send Status:\t");
+    // Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
 // ******************************************
