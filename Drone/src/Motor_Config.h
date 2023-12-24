7df249e2a95728dc73d5a6f0e08fec1abd6f5a1a
duycuong <voduycuonggli@gmail.com>
2023-12-24 16:33:41 +0700

    adjust Run_Motor()

--- Drone/src/Motor_Config.h
+++ Drone/src/Motor_Config.h
@@ -1,20 +1,23 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
 
-#define MOTOR_1_PIN 32 // Pin 32 attached to ESC signal pin
-#define MOTOR_2_PIN 33 // Pin 33 attached to ESC signal pin
-#define MOTOR_3_PIN 18 // Pin 18 attached to ESC signal pin
-#define MOTOR_4_PIN 19 // Pin 19 attached to ESC signal pin
+#define MOTOR_1_PIN 32 // Pin 32 attached to ESC signal pin (Rear Right)
+#define MOTOR_2_PIN 33 // Pin 33 attached to ESC signal pin (Front Right)
+#define MOTOR_3_PIN 18 // Pin 18 attached to ESC signal pin (Rear Left)
+#define MOTOR_4_PIN 19 // Pin 19 attached to ESC signal pin (Front Left)
 
 #define MAX_SIGNAL 2000 // Parameter required for ESC definition
 #define MIN_SIGNAL 1000 // Parameter required for the ESC definition
 
 extern int CtrlPWM;
+extern double pid_output_x;
+extern double pid_output_y;
+extern double pid_output_z;
 
-Servo ESC_1; // Define ESC
-Servo ESC_2; // Define ESC
-Servo ESC_3; // Define ESC
-Servo ESC_4; // Define ESC
+Servo ESC_1; // Define ESC (Rear Right)
+Servo ESC_2; // Define ESC (Front Right)
+Servo ESC_3; // Define ESC (Rear Left)
+Servo ESC_4; // Define ESC (Front Left)
 
 void Init_ESC()
 {
@@ -27,8 +30,8 @@ void Init_ESC()
 void Run_Motor()
 {
     // Send command to ESCs
-    ESC_1.write(CtrlPWM + pid_output_x + pid_output_y - pid_output_z);
-    ESC_2.write(CtrlPWM - pid_output_x + pid_output_y + pid_output_z);
-    ESC_3.write(CtrlPWM - pid_output_x - pid_output_y - pid_output_z);
-    ESC_4.write(CtrlPWM + pid_output_x - pid_output_y + pid_output_z);
+    ESC_1.write(CtrlPWM - pid_output_x + pid_output_y - pid_output_z);
+    ESC_2.write(CtrlPWM + pid_output_x + pid_output_y + pid_output_z);
+    ESC_3.write(CtrlPWM - pid_output_x - pid_output_y + pid_output_z);
+    ESC_4.write(CtrlPWM + pid_output_x - pid_output_y - pid_output_z);
 }
